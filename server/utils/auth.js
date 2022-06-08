e440e410b1e854c339522fe53adf8a44e3aa66d0
ngranzow <nate.granzow@gmail.com>
2022-06-07 23:00:47 -0500

    fixed Apollo graphql connection error

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,11 +4,11 @@ const jwt = require('jsonwebtoken');
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
-module.exports = {
+module.export = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
